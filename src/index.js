d0f23cb6903be43299d1569fb6ac12b0e8154fec
JHJ <hy9458@duksung.ac.kr>
2024-10-10 10:47:13 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// computes a factorial of a given positive integer
+function factorial(n) {
+
+    if( n < 0 ) {       // 5! = 5*4*3*2*1
+        throw new Error('n must be a positive integer');
+    }
+
+
+    if( n === 0 ) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);        // 재귀적 호출 
+}
+
+module.exports = factorial;         // 외부에서 사용할 수 있게 만듦
